25ffd7f8510bbcf465aa50b619890e99c85c5656
Zachary Gottschall <zachary.m.gottschall.20@dartmouth.edu>
2019-11-07 15:48:10 -0500

    update redirect url

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/login/#' +
+        res.redirect('https://cs98-duet.surge.sh/login/#' +
             querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
