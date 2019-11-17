7f2cea703f4d653b870a48d0f4162f7c5e0016db
Zachary Gottschall <zachary.m.gottschall.20@dartmouth.edu>
2019-11-17 14:22:34 -0500

    redirect to local

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('https://cs98-duet-surge.sh/login/#' +
+        res.redirect('http://localhost:3000/login/#' +
             querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
