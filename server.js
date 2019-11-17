e68861b5fb9ff651c816cf56932308a57682b4f6
Zachary Gottschall <zachary.m.gottschall.20@dartmouth.edu>
2019-11-17 16:01:09 -0500

    update redirect

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = 'c8befa57bfe848f7a9fd44e09e5450d6';
 var client_secret = '00812ab3a29d4c6e869542c7cc762d54';
-var redirect_uri = 'https://cs98-duet-auth-server.herokuapp.com/callback';
+var redirect_uri = 'http://localhost:8081/callback' // 'https://cs98-duet-auth-server.herokuapp.com/callback';
 
 /**
  * Generates a random string containing numbers and letters
@@ -104,7 +104,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/login/#' +
+        res.redirect('https://cs98-duet.surge.sh/login/#' +
             querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
@@ -145,3 +145,5 @@ app.get('/refresh_token', function(req, res) {
 
 const port = process.env.PORT || 8081;
 app.listen(port);
+
+console.log('listening on port', port)
